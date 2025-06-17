fa4a278690056b5ba82f0462e1a0dcb153d6295d
Risekou1 <temka4705@gmail.com>
2025-06-17 23:33:31 +0900

    temuujin

---
+++ Arrays_Hashing/longest_cons_seq.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution1:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        return 
+    
+
+class Solution2:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        return 
+
+
+
+if __name__ == "__main__":
+    nums = [100,4,200,1,3,2] # output - 4
+
+    solu1 = Solution1()
+    output1 = solu1.productExceptSelf(nums)
+
+    solu2 = Solution2()
+    output2 = solu2.productExceptSelf(nums)
+
+
+
+    print(output1)
--- Arrays_Hashing/product_of_array_except_itself.py
+++ Arrays_Hashing/product_of_array_except_itself.py
@@ -1,14 +1,64 @@
 from typing import List
 
-class Solution1:
+class Solution1:  # brute force.
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        print(nums)
+        output = []
+        for i in range(len(nums)):
+            prod = 1
+            for j in range(len(nums)):
+                if i == j :
+                    continue
+                prod *= nums[j]
+            output.append(prod)
+        return output
 
 
+class Solution2:  # suffix and prefix products.
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pre = [0]*n
+        suf = [0]*n
+        pre[0] = 1
+        suf[n-1] = 1
 
+        for i in range(1,n):
+            pre[i] = pre[i-1] * nums[i-1]
+        for j in range(n-2,-1,-1):
+            suf[j] = suf[j+1] * nums[j+1]
+
+        output = [pre[i] * suf[i] for i in range(n)]
+
+        return output
+    
+class Solution3:  # suffix and prefix products but space complexity is O(1)
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        output = [1]*n
+
+        cur = 1
+        for i in range(n):
+            output[i] *= cur
+            cur *= nums[i]
+        
+        cur = 1
+        for i in range(n-1,-1,-1):
+            output[i] *= cur
+            cur *= nums[i]
+
+        return output
 
 
 if __name__ == "__main__":
+    nums = [1,2,3,4]  # [24,12,8,6]
+
     solu1 = Solution1()
-    nums = [1,2,3,4]
-    solu1.productExceptSelf(nums)
+    output1 = solu1.productExceptSelf(nums)
+
+    solu2 = Solution2()
+    output2 = solu2.productExceptSelf(nums)
+
+
+    solu3 = Solution3()
+    output3 = solu3.productExceptSelf(nums)
+
+    print(output3)
