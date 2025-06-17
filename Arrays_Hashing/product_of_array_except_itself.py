093da0df1a47076083024152b828af228176e74b
Risekou1 <temka4705@gmail.com>
2025-06-17 18:46:24 +0900

    a

---
+++ Arrays_Hashing/product_of_array_except_itself.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution1:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        print(nums)
+
+
+
+
+
+if __name__ == "__main__":
+    solu1 = Solution1()
+    nums = [1,2,3,4]
+    solu1.productExceptSelf(nums)
